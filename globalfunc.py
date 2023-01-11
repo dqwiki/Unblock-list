48e03cfd62c853135138893a7871b64e36c802bc
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2023-01-10 19:20:21 -0500

    brs

--- globalfunc.py
+++ globalfunc.py
@@ -140,7 +140,7 @@ def formatrow(block,appealtime,lastedit,type):
     if type=="auto":style='|- style="background-color:#ADD8E6"\n'
     if type=="username":style='|- style="background-color:#FFEFDB"\n'
     if type=="hold":style='|- style="background-color:#CC99CC"\n'
-    return style+"|[[User talk:"+block['user']+"|"+block['user']+"]]\n|"+appealtime+"\n| Admin: "+block['blockadmin']+"\n Date: "+block['blockdate']+"\n Reason:"+block['blockreason']+"\n Length:"+block['blocklength']+"\n|"+lastedit['user']+"\n|"+lastedit['timestamp']+"\n"
+    return style+"|[[User talk:"+block['user']+"|"+block['user']+"]]\n|"+appealtime+"\n|Admin: "+block['blockadmin']+"<br>Date: "+block['blockdate']+"<br>Reason: "+block['blockreason']+"<br>Length: "+block['blocklength']+"\n|"+lastedit['user']+"\n|"+lastedit['timestamp']+"\n"
 def runCategory(cat,type,table):
     alltable=""
     ulist = getMembers(cat)
