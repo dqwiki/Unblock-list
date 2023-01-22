62b59d5b602a9e1d0dbb52288994abb346fc1567
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2023-01-22 15:10:14 -0500

    Again per Tamzin

--- globalfunc.py
+++ globalfunc.py
@@ -140,7 +140,7 @@ def formatrow(block,appealtime,lastedit,type):
     if type=="auto":style='|- style="background-color:#ADD8E6"\n'
     if type=="username":style='|- style="background-color:#FFEFDB"\n'
     if type=="hold":style='|- style="background-color:#CC99CC"\n'
-    return style+"\n|"+appealtime+"|[[User talk:"+block['user']+"|"+block['user']+"]]\n"+"|Admin: "+block['blockadmin']+"<br>Date: "+block['blockdate']+"<br>Reason: "+block['blockreason']+"<br>Length: "+block['blocklength']+"\n|"+lastedit['user']+"\n|"+lastedit['timestamp']+"\n"
+    return style+"|"+appealtime+"|[[User talk:"+block['user']+"|"+block['user']+"]]\n"+"|Admin: "+block['blockadmin']+"<br>Date: "+block['blockdate']+"<br>Reason: "+block['blockreason']+"<br>Length: "+block['blocklength']+"\n|"+lastedit['user']+"\n|"+lastedit['timestamp']+"\n"
 def runCategory(cat,type,table):
     alltable=""
     ulist = getMembers(cat)
@@ -156,7 +156,7 @@ def runCategory(cat,type,table):
     return alltable
 table = """
 {|class="wikitable sortable" width="100%"
-!User!!Request time!!Block info!!Last user edit!!Timestamp
+!Request time!!User!!Block info!!Last user edit!!Timestamp
 """
 table+=runCategory("Requests for username changes when blocked‎","username",table)
 table+=runCategory("Requests for unblock","normal",table)
